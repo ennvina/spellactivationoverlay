6e051e295d91343975f704cea88a555cc2c63e6a
ennvina <vinnywow@aim.com>
2022-07-30 01:56:27 +0200

    Try to use 3 effects for 3 stacks of Molten Core

--- classes/warlock.lua
+++ classes/warlock.lua
@@ -4,7 +4,7 @@ local function registerMolenCore(self, baseName, spellID)
     self:RegisterAura(baseName.."_1", 1, spellID, "molten_core", "Left", 1, 255, 255, 255);
     self:RegisterAura(baseName.."_2", 2, spellID, "molten_core", "Left + Right (Flipped)", 1, 255, 255, 255);
     self:RegisterAura(baseName.."_3", 3, spellID, "molten_core", "Left + Right (Flipped)", 1, 255, 255, 255);
---    self:RegisterAura(baseName.."_3_bis", 3, spellID, "impact", "Top", 1, 255, 255, 255);
+    self:RegisterAura(baseName.."_3_bis", 3, spellID, "impact", "Top", 1, 255, 255, 255);
 --    self:RegisterAura(baseName.."_3_bis", 3, spellID, "lock_and_load", "Top", 1, 255, 255, 255);
 end
 
