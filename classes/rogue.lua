cb8ffabd13633065ff7a4b33dfcc8eef2200caaf
ennvina <vinnywow@aim.com>
2022-07-31 10:41:51 +0200

    Added Rogue class, which has no SAOs

---
+++ classes/rogue.lua
@@ -0,0 +1,9 @@
+local AddonName, SAO = ...
+
+local function registerAuras(self)
+    -- A rogue's gonna do what a rogue's gonna do
+end
+
+SAO.Class["ROGUE"] = {
+    ["Register"] = registerAuras,
+}
