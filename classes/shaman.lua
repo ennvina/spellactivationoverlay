147165419c037638f7691ad3970bf5eea690e5cd
ennvina <vinnywow@aim.com>
2022-07-31 23:03:06 +0200

    First draft of Elemental Focus

--- classes/shaman.lua
+++ classes/shaman.lua
@@ -1,6 +1,11 @@
 local AddonName, SAO = ...
 
 local function registerAuras(self)
+    -- Elemental Focus
+    self:RegisterAura("elemental_focus_1", 1, 16246, "natures_grace", "Left", 1, 255, 255, 255);
+    self:RegisterAura("elemental_focus_2", 2, 16246, "natures_grace", "Left + Right (Flipped)", 1, 255, 255, 255);
+
+    -- Maelstrom Weapon
     self:RegisterAura("maelstrom_weapoon_1", 1, 53817, 1028136, "Top", 1, 255, 255, 255);
     self:RegisterAura("maelstrom_weapoon_2", 2, 53817, 1028137, "Top", 1, 255, 255, 255);
     self:RegisterAura("maelstrom_weapoon_3", 3, 53817, 1028138, "Top", 1, 255, 255, 255);
