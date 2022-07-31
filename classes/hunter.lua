491b23964ad41558b07813ff4466ea56948f9f2b
ennvina <vinnywow@aim.com>
2022-07-31 11:59:48 +0200

    First draft of Lock and Load

--- classes/hunter.lua
+++ classes/hunter.lua
@@ -2,6 +2,7 @@ local AddonName, SAO = ...
 
 local function registerAuras(self)
     self:RegisterAura("improved_steady_shot", 0, 53220, "master_marksman", "Top", 1, 255, 255, 255);
+    self:RegisterAura("lock_and_load", 0, 56453, "lock_and_load", "Top", 1, 255, 255, 255);
 end
 
 SAO.Class["HUNTER"] = {
