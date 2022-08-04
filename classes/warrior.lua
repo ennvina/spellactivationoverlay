fec843d4944620a3e404b40148f1defed07f19b2
ennvina <vinnywow@aim.com>
2022-08-05 01:53:00 +0200

    All warrior SAOs automatically pulse

--- classes/warrior.lua
+++ classes/warrior.lua
@@ -1,9 +1,9 @@
 local AddonName, SAO = ...
 
 local function registerAuras(self)
-    self:RegisterAura("bloodsurge", 0, 46916, 449487, "Top", 1, 255, 255, 255);
-    self:RegisterAura("sudden_death", 0, 52437, 449493, "Left + Right (Flipped)", 1, 255, 255, 255);
-    self:RegisterAura("sword_and_board", 0, 50227, 449494, "Left + Right (Flipped)", 1, 255, 255, 255);
+    self:RegisterAura("bloodsurge", 0, 46916, 449487, "Top", 1, 255, 255, 255, true);
+    self:RegisterAura("sudden_death", 0, 52437, 449493, "Left + Right (Flipped)", 1, 255, 255, 255, true);
+    self:RegisterAura("sword_and_board", 0, 50227, 449494, "Left + Right (Flipped)", 1, 255, 255, 255, true);
 end
 
 SAO.Class["WARRIOR"] = {
