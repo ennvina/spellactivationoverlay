4c4d340c016526b196a5b6ea621176fd09e381ea
ennvina <vinnywow@aim.com>
2022-08-05 01:52:34 +0200

    Maelstrom Weapon pulses only at 5 stacks

--- classes/shaman.lua
+++ classes/shaman.lua
@@ -2,15 +2,15 @@ local AddonName, SAO = ...
 
 local function registerAuras(self)
     -- Elemental Focus
-    self:RegisterAura("elemental_focus_1", 1, 16246, "echo_of_the_elements", "Left", 1, 255, 255, 255);
-    self:RegisterAura("elemental_focus_2", 2, 16246, "echo_of_the_elements", "Left + Right (Flipped)", 1, 255, 255, 255);
+    self:RegisterAura("elemental_focus_1", 1, 16246, "echo_of_the_elements", "Left", 1, 255, 255, 255, true);
+    self:RegisterAura("elemental_focus_2", 2, 16246, "echo_of_the_elements", "Left + Right (Flipped)", 1, 255, 255, 255, true);
 
     -- Maelstrom Weapon
-    self:RegisterAura("maelstrom_weapoon_1", 1, 53817, 1028136, "Top", 1, 255, 255, 255);
-    self:RegisterAura("maelstrom_weapoon_2", 2, 53817, 1028137, "Top", 1, 255, 255, 255);
-    self:RegisterAura("maelstrom_weapoon_3", 3, 53817, 1028138, "Top", 1, 255, 255, 255);
-    self:RegisterAura("maelstrom_weapoon_4", 4, 53817, 1028139, "Top", 1, 255, 255, 255);
-    self:RegisterAura("maelstrom_weapoon_5", 5, 53817, 450927, "Top", 1, 255, 255, 255);
+    self:RegisterAura("maelstrom_weapoon_1", 1, 53817, 1028136, "Top", 1, 255, 255, 255, false);
+    self:RegisterAura("maelstrom_weapoon_2", 2, 53817, 1028137, "Top", 1, 255, 255, 255, false);
+    self:RegisterAura("maelstrom_weapoon_3", 3, 53817, 1028138, "Top", 1, 255, 255, 255, false);
+    self:RegisterAura("maelstrom_weapoon_4", 4, 53817, 1028139, "Top", 1, 255, 255, 255, false);
+    self:RegisterAura("maelstrom_weapoon_5", 5, 53817, 450927, "Top", 1, 255, 255, 255, true);
 end
 
 SAO.Class["SHAMAN"] = {
