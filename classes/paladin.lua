e94d064c8952521c738a30cad50649e0a98308f7
ennvina <vinnywow@aim.com>
2022-08-04 02:01:32 +0200

    First draft of Infusion of Light

--- classes/paladin.lua
+++ classes/paladin.lua
@@ -1,7 +1,14 @@
 local AddonName, SAO = ...
 
 local function registerAuras(self)
+    -- Art of War
     self:RegisterAura("art_of_war", 0, 59578, 450913, "Left + Right (Flipped)", 1, 255, 255, 255);
+
+    -- Infusion of Light, 1/2 talent points
+    self:RegisterAura("infusion_of_light_low", 0, 53672, "daybreak", "Left + Right (Flipped)", 1, 255, 255, 255);
+
+    -- Infusion of Light, 2/2 talent points
+    self:RegisterAura("infusion_of_light_high", 0, 54149, "daybreak", "Left + Right (Flipped)", 1, 255, 255, 255);
 end
 
 SAO.Class["PALADIN"] = {
