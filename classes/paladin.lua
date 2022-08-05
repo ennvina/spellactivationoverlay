a9faf3e88deba92fa2d13e3b417d1a5f5eb62b12
ennvina <vinnywow@aim.com>
2022-08-05 17:42:31 +0200

    Texture names for paladin

--- classes/paladin.lua
+++ classes/paladin.lua
@@ -2,7 +2,7 @@ local AddonName, SAO = ...
 
 local function registerAuras(self)
     -- Art of War
-    self:RegisterAura("art_of_war", 0, 59578, 450913, "Left + Right (Flipped)", 1, 255, 255, 255, true);
+    self:RegisterAura("art_of_war", 0, 59578, "art_of_war", "Left + Right (Flipped)", 1, 255, 255, 255, true);
 
     -- Infusion of Light, 1/2 talent points
     self:RegisterAura("infusion_of_light_low", 0, 53672, "daybreak", "Left + Right (Flipped)", 1, 255, 255, 255, true);
