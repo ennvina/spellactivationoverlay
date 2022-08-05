9695a88b08d6cb1453c5d9c7db713a86e8908f07
ennvina <vinnywow@aim.com>
2022-08-05 18:13:21 +0200

    Texture names for warrior

--- classes/warrior.lua
+++ classes/warrior.lua
@@ -1,9 +1,9 @@
 local AddonName, SAO = ...
 
 local function registerAuras(self)
-    self:RegisterAura("bloodsurge", 0, 46916, 449487, "Top", 1, 255, 255, 255, true);
-    self:RegisterAura("sudden_death", 0, 52437, 449493, "Left + Right (Flipped)", 1, 255, 255, 255, true);
-    self:RegisterAura("sword_and_board", 0, 50227, 449494, "Left + Right (Flipped)", 1, 255, 255, 255, true);
+    self:RegisterAura("bloodsurge", 0, 46916, "blood_surge", "Top", 1, 255, 255, 255, true);
+    self:RegisterAura("sudden_death", 0, 52437, "sudden_death", "Left + Right (Flipped)", 1, 255, 255, 255, true);
+    self:RegisterAura("sword_and_board", 0, 50227, "sword_and_board", "Left + Right (Flipped)", 1, 255, 255, 255, true);
 end
 
 SAO.Class["WARRIOR"] = {
