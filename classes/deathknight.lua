94064b9dfdc907ddf466030609d16b828f2eff9e
ennvina <vinnywow@aim.com>
2022-08-05 17:33:05 +0200

    Texture names for DK

--- classes/deathknight.lua
+++ classes/deathknight.lua
@@ -1,8 +1,8 @@
 local AddonName, SAO = ...
 
 local function registerAuras(self)
-    self:RegisterAura("rime", 0, 59052, 450930, "Top", 1, 255, 255, 255, true);
-    self:RegisterAura("killing_machine", 0, 51124, 458740, "Left + Right (Flipped)", 1, 255, 255, 255, true);
+    self:RegisterAura("rime", 0, 59052, "rime", "Top", 1, 255, 255, 255, true);
+    self:RegisterAura("killing_machine", 0, 51124, "killing_machine", "Left + Right (Flipped)", 1, 255, 255, 255, true);
 end
 
 SAO.Class["DEATHKNIGHT"] = {
