ce457cfec4b38e533b47d83c32e908ccf8dab5e0
ennvina <vinnywow@aim.com>
2022-08-26 17:37:40 +0200

    Events are now registered on-demand for each class

--- classes/deathknight.lua
+++ classes/deathknight.lua
@@ -1,10 +1,10 @@
 local AddonName, SAO = ...
 
-local function registerAuras(self)
+local function registerClass(self)
     self:RegisterAura("rime", 0, 59052, "rime", "Top", 1, 255, 255, 255, true);
     self:RegisterAura("killing_machine", 0, 51124, "killing_machine", "Left + Right (Flipped)", 1, 255, 255, 255, true);
 end
 
 SAO.Class["DEATHKNIGHT"] = {
-    ["Register"] = registerAuras,
+    ["Register"] = registerClass,
 }
