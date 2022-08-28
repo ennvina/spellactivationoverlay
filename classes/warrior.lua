6228cee894d55c1093757eaa69c42da40d258034
ennvina <vinnywow@aim.com>
2022-08-29 00:58:49 +0200

    Warrior GAB for Bloodsurge

--- classes/warrior.lua
+++ classes/warrior.lua
@@ -1,7 +1,7 @@
 local AddonName, SAO = ...
 
 local function registerClass(self)
-    self:RegisterAura("bloodsurge", 0, 46916, "blood_surge", "Top", 1, 255, 255, 255, true);
+    self:RegisterAura("bloodsurge", 0, 46916, "blood_surge", "Top", 1, 255, 255, 255, true, { GetSpellInfo(1464) });
     self:RegisterAura("sudden_death", 0, 52437, "sudden_death", "Left + Right (Flipped)", 1, 255, 255, 255, true);
     self:RegisterAura("sword_and_board", 0, 50227, "sword_and_board", "Left + Right (Flipped)", 1, 255, 255, 255, true, { GetSpellInfo(23922) });
 end
