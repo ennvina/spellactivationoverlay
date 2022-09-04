d45df402bc8068f123bfcd4560c63586f3d52164
ennvina <vinnywow@aim.com>
2022-09-04 05:30:33 +0200

    First draft of options panel

---
+++ options/InterfaceOptionsPanels.lua
@@ -0,0 +1,36 @@
+local Addon, SAO = ...
+
+function SpellActivationOverlayOptionsPanel_Init(self)
+    local opacitySlider = SpellActivationOverlayOptionsPanelSpellAlertOpacitySlider;
+    opacitySlider.Text:SetText("SPELL_ALERT_OPACITY");
+    opacitySlider:SetMinMaxValues(0, 1);
+    opacitySlider:SetValueStep(0.05);
+    opacitySlider:SetValue(SpellActivationOverlayDB.alert.opacity);
+
+    local glowingButtonCheckbox = SpellActivationOverlayOptionsPanelGlowingButtons;
+    glowingButtonCheckbox.Text:SetText("Glowing Buttons");
+    glowingButtonCheckbox:SetChecked(SpellActivationOverlayDB.glow.enabled);
+end
+
+function SpellActivationOverlayOptionsPanel_OnLoad(self)
+    self.name = Addon;
+
+    local opacitySlider = SpellActivationOverlayOptionsPanelSpellAlertOpacitySlider;
+    local glowingButtonCheckbox = SpellActivationOverlayOptionsPanelGlowingButtons;
+
+    self.okay = function(self)
+        SpellActivationOverlayDB.alert.opacity = opacitySlider:GetValue();
+
+        SpellActivationOverlayDB.glow.enabled = glowingButtonCheckbox:GetChecked();
+
+        -- TODO also apply values in the SAO/GAB engine
+    end
+
+    self.cancel = function(self)
+        opacitySlider:SetValue(SpellActivationOverlayDB.alert.opacity);
+
+        glowingButtonCheckbox:SetChecked(SpellActivationOverlayDB.glow.enabled);
+    end
+
+    InterfaceOptions_AddCategory(self);
+end
---
+++ options/db.lua
@@ -0,0 +1,29 @@
+Addon, SAO =...
+
+-- Load database and use default values if needed
+function SAO.LoadDB(self)
+    local currentversion = 060;
+    local db = SpellActivationOverlayDB or {};
+
+    if not db.alert then
+        db.alert = {};
+        db.alert.enabled = true;
+        db.alert.opacity = 0.65;
+    end
+
+    if not db.glow then
+        db.glow = {};
+        db.glow.enabled = true;
+    end
+
+    db.version = currentversion;
+    SpellActivationOverlayDB = db;
+end
+
+local frame = CreateFrame("Frame", "SpellActivationOverlayDBLoader");
+frame:RegisterEvent("VARIABLES_LOADED");
+frame:SetScript("OnEvent", function (event)
+    SAO.LoadDB();
+    SpellActivationOverlayOptionsPanel_Init();
+    frame:UnregisterEvent("VARIABLES_LOADED");
+end);
