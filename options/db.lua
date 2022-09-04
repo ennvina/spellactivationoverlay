74c1698e439ddaa78b79f79042260a87e36731de
ennvina <vinnywow@aim.com>
2022-09-04 17:38:33 +0200

    Auto-correct database corruptions

--- options/db.lua
+++ options/db.lua
@@ -7,14 +7,26 @@ function SAO.LoadDB(self)
 
     if not db.alert then
         db.alert = {};
+    end
+    if (type(db.alert.enabled) == "nil" and type(db.alert.opacity) == "nil") then
         db.alert.enabled = true;
         db.alert.opacity = 1;
+    elseif (type(db.alert.opacity) == "nil") then
+        db.alert.opacity = db.alert.enabled and 1 or 0;
+    elseif (type(db.alert.enabled) == "nil") then
+        db.alert.enabled = db.alert.opacity > 0;
+    end
+    if (type(db.alert.offset) == "nil") then
         db.alert.offset = 0;
+    end
+    if (type(db.alert.scale) == "nil") then
         db.alert.scale = 1;
     end
 
     if not db.glow then
         db.glow = {};
+    end
+    if (type(db.glow.enabled) == "nil") then
         db.glow.enabled = true;
     end
 
