03b2e11934e731b8d086e73057fc64dc2bc21eb5
xHashii <clownprince117@gmail.com>
2022-09-05 22:52:39 +0300

    Fixed some syntax

--- classes/hunter.lua
+++ classes/hunter.lua
@@ -3,7 +3,7 @@ local AddonName, SAO = ...
 local function registerClass(self)
     local aimedShot = 19434;
     local arcaneShot = 3044;
-    local killShot  = 61006;
+    local kill_shot  = 61006;
     local chimeraShot = 53209;
     local explosiveShot = 53301;
     local counterattack = 19306;
@@ -32,8 +32,8 @@ local function registerClass(self)
     self:RegisterCounter("counterattack"); -- Must match name from above call
 
     -- Kill Shot, execute based on 20% and below
-    self:RegisterAura("killShot", 0, killShot, nil, "", 0, 0, 0, 0, false, { {GetSpellInfo(killShot)} });
-    self:RegisterCounter("killShot");
+    self:RegisterAura("kill_shot", 0, kill_shot, nil, "", 0, 0, 0, 0, false, { (GetSpellInfo(killShot)) });
+    self:RegisterCounter("kill_shot");
 end
 
 SAO.Class["HUNTER"] = {
--- classes/warrior.lua
+++ classes/warrior.lua
@@ -1,11 +1,11 @@
 local AddonName, SAO = ...
 
 local function registerClass(self)
-    local tasteforblood = GetSpellInfo(60503);
-    local overpower = GetSpellInfo(7384);
+    local tasteforBlood = GetSpellInfo(60503);
+    local overpower = GetSpellInfo(7384); -- Unused as of now, might be used in the future.
     local execute = GetSpellInfo(5308);
     local revenge = GetSpellInfo(6572);
-    local victoryrush = GetSpellInfo(34428);
+    local victoryRush = GetSpellInfo(34428);
 
     self:RegisterAura("bloodsurge", 0, 46916, "blood_surge", "Top", 1, 255, 255, 255, true, { GetSpellInfo(1464) });
     self:RegisterAura("sudden_death", 0, 52437, "sudden_death", "Left + Right (Flipped)", 1, 255, 255, 255, true, { execute });
@@ -24,8 +24,8 @@ local function registerClass(self)
     self:RegisterCounter("revenge"); -- Must match name from above call
 
     -- Victory Rush
-    self:RegisterAura("victoryrush", 0, victoryrush, nil, "", 0, 0, 0, 0, false, { victoryrush });
-    self:RegisterCounter("victoryrush"); -- Must match name from above call
+    self:RegisterAura("victoryRush", 0, victoryRush, nil, "", 0, 0, 0, 0, false, { victoryRush });
+    self:RegisterCounter("victoryRush"); -- Must match name from above call
 end
 
 SAO.Class["WARRIOR"] = {
