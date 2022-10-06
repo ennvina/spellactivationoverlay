28dfce06bf35152bd438d9dec72c088b24327906
ennvina <vinnywow@aim.com>
2022-10-06 23:24:27 +0200

    Cleanup priest code

--- classes/priest.lua
+++ classes/priest.lua
@@ -11,19 +11,13 @@ local function registerClass(self)
     local ghAndPoh = { (GetSpellInfo(2060)), (GetSpellInfo(596)) }
 
     -- Serendipity with 1 talent point out of 3
---    self:RegisterAura("serendipity_low_1", 1, serendipityBuff1, "serendipity", "Top", 0.25, 255, 255, 255, true, ghAndPoh);
---    self:RegisterAura("serendipity_low_2", 2, serendipityBuff1, "serendipity", "Top", 0.5, 255, 255, 255, true, ghAndPoh);
-    self:RegisterAura("serendipity_low_3", 3, serendipityBuff1, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
+    self:RegisterAura("serendipity_low", 3, serendipityBuff1, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
 
     -- Serendipity with 2 talent points out of 3
---    self:RegisterAura("serendipity_medium_1", 1, serendipityBuff2, "serendipity", "Top", 0.25, 255, 255, 255, true, ghAndPoh);
---    self:RegisterAura("serendipity_medium_2", 2, serendipityBuff2, "serendipity", "Top", 0.5, 255, 255, 255, true, ghAndPoh);
-    self:RegisterAura("serendipity_medium_3", 3, serendipityBuff2, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
+    self:RegisterAura("serendipity_medium", 3, serendipityBuff2, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
 
     -- Serendipity with 3 talent points out of 3
---    self:RegisterAura("serendipity_high_1", 1, serendipityBuff3, "serendipity", "Top", 0.25, 255, 255, 255, true, ghAndPoh);
---    self:RegisterAura("serendipity_high_2", 2, serendipityBuff3, "serendipity", "Top", 0.5, 255, 255, 255, true, ghAndPoh);
-    self:RegisterAura("serendipity_high_3", 3, serendipityBuff3, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
+    self:RegisterAura("serendipity_high", 3, serendipityBuff3, "serendipity", "Top", 1, 255, 255, 255, true, ghAndPoh);
 
     -- Add option links during registerClass(), not because loadOptions() which would be loaded only when the options panel is opened
     self:AddGlowingLink(serendipityBuff3, serendipityBuff1);
@@ -31,7 +25,6 @@ local function registerClass(self)
 end
 
 local function loadOptions(self)
--- Smite (Surge of Light), Flash Heal (Surge of Light), Greater Heal (3 stacks of Serendipity), Prayer of Healing (3 stacks of Serendipity)
     local smite = 585;
     local flashHeal = 2061;
     local greaterHeal = 2060;
@@ -40,8 +33,6 @@ local function loadOptions(self)
     local surgeOfLightBuff = 33151;
     local surgeOfLightTalent = 33150;
 
---    local serendipityBuff1 = 63731;
---    local serendipityBuff2 = 63735;
     local serendipityBuff3 = 63734;
     local serendipityTalent = 63730;
 
