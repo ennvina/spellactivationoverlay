7455f822b1ef3c5c2f1a7920bdb3f46b0d10b789
ennvina <vinnywow@aim.com>
2022-10-06 23:59:29 +0200

    Cleanup Death Knight's Rune Strike text

--- classes/deathknight.lua
+++ classes/deathknight.lua
@@ -14,7 +14,7 @@ end
 
 local function loadOptions(self)
     local runeStrike = 56815;
-    self:AddGlowingOption(GetSpellInfo(runeStrike), runeStrike, runeStrike);
+    self:AddGlowingOption(nil, runeStrike, runeStrike);
 
     local rimeBuff = 59052;
     local rimeTalent = 49188;
