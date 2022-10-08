664e50b084f4ffe34db49ccbbb2fd104475ed69c
ennvina <vinnywow@aim.com>
2022-10-08 16:08:39 +0200

    Overlay option with stacks defaults to 0 stacks

--- components/overlay.lua
+++ components/overlay.lua
@@ -10,12 +10,33 @@ function SAO.GetActiveOverlay(self, spellID)
     return self.ActiveOverlays[spellID] ~= nil;
 end
 
+-- Search in overlay options if the specified auraID should be discarded
+-- By default, do *not* discard
+-- This happens e.g., if there is no option for this auraID
+local function discardedByOverlayOption(self, auraID, stacks)
+    local overlayOptions = self:GetOverlayOptions(auraID);
+
+    if (not overlayOptions) then
+        return false; -- By default, do not discard
+    end
+
+    -- Look for option in the exact stack count
+    if (type(overlayOptions[stacks]) ~= "nil") then
+        return not overlayOptions[stacks];
+    end
+
+    -- Look for a default option as if stacks == 0
+    if (stacks and stacks > 0 and type(overlayOptions[0]) ~= "nil") then
+        return not overlayOptions[0];
+    end
+
+    return false; -- By default, do not discard
+end
+
 -- Add or refresh an overlay
 function SAO.ActivateOverlay(self, stacks, spellID, texture, positions, scale, r, g, b, autoPulse, forcePulsePlay)
     if (texture) then
-        local overlayOptions = self:GetOverlayOptions(spellID);
-        if (overlayOptions and type(overlayOptions[stacks] ~= "nil") and not overlayOptions[stacks]) then
-            -- Option tells to discard this overlay
+        if (discardedByOverlayOption(self, spellID, stacks)) then
             return;
         end
         if (type(forcePulsePlay) == 'table') then -- Hack to avoid glowIDs to be treated as forcePulsePlay
