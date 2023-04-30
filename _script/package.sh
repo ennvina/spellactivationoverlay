14cbcdde3a021b05f5c33a16543644c79b07ae22
ennvina <vinnywow@aim.com>
2023-04-30 04:30:20 +0200

    Check version consistency when packaging

--- _script/package.sh
+++ _script/package.sh
@@ -2,14 +2,31 @@
 CWD=$(cd -P "$(dirname "$0")" && pwd)
 
 bye() {
-    echo "$1" >&2;
+    echo "$1" >&2
+    stat=($(</proc/$$/stat))
+    [ ${stat[3]} -eq 1 ] && read # Pause if called in separate window
     exit 1
 }
 
 cd "$CWD"/.. || bye "Cannot go to parent directory"
 
-VERSION=$(grep -i '^##[[:space:]]*version:' ./SpellActivationOverlay.toc | grep -o '[0-9].*')
-([ -z "$VERSION" ] || [[ "$VERSION" =~ \n ]]) && bye "Cannot retrieve version from TOC file"
+# Retrieve version and check consistency
+VERSION_TOC_VERSION=$(grep -i '^##[[:space:]]*version:' ./SpellActivationOverlay.toc | grep -o '[0-9].*')
+VERSION_TOC_TITLE=$(grep -i '^##[[:space:]]*title:' ./SpellActivationOverlay.toc | grep -o '|c........[0-9].*|r' | grep -o '[0-9]*\.[^|]*')
+VERSION_CHANGELOG=$(grep -m1 -o '^#### v[^[:space:]]*' ./changelog.md | grep -o '[0-9].*')
+if [ -z "$VERSION_TOC_VERSION" ] || [[ "$VERSION_TOC_VERSION" =~ \n ]] \
+|| [ -z "$VERSION_TOC_TITLE" ] || [[ "$VERSION_TOC_TITLE" =~ \n ]]
+then
+    bye "Cannot retrieve version from TOC file"
+fi
+if [ -z "$VERSION_CHANGELOG" ] || [[ "$VERSION_CHANGELOG" =~ \n ]]
+then
+    bye "Cannot retrieve version from ChangeLog file"
+fi
+if [ "$VERSION_TOC_VERSION" != "$VERSION_TOC_TITLE" ] || [ "$VERSION_TOC_VERSION" != "$VERSION_CHANGELOG" ]
+then
+    bye "Versions do not match: $VERSION_TOC_VERSION (toc, version) vs. $VERSION_TOC_TITLE (toc, title) vs. $VERSION_CHANGELOG (changelog)"
+fi
 
 # Release wrath version
 rm -rf ./_release/wrath || bye "Cannot clean wrath directory"
@@ -17,7 +34,7 @@ mkdir -p ./_release/wrath/SpellActivationOverlay || bye "Cannot create wrath dir
 cp -R changelog.md LICENSE SpellActivationOverlay.* classes components options textures ./_release/wrath/SpellActivationOverlay/ || bye "Cannot copy wrath files"
 cd ./_release/wrath || bye "Cannot cd to wrath directory"
 echo -n "Zipping wrath directory... "
-"$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION"-wrath.zip SpellActivationOverlay || bye "Cannot zip wrath directory"
+"$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION_TOC_VERSION"-wrath.zip SpellActivationOverlay || bye "Cannot zip wrath directory"
 echo
 explorer . # || bye "Cannot open explorer to wrath directory"
 
@@ -72,6 +89,6 @@ do
 done
 echo
 echo -n "Zipping vanilla directory... "
-"$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION"-vanilla.zip SpellActivationOverlay || bye "Cannot zip vanilla directory"
+"$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION_TOC_VERSION"-vanilla.zip SpellActivationOverlay || bye "Cannot zip vanilla directory"
 echo
 explorer . # || bye "Cannot open explorer to vanilla directory"
