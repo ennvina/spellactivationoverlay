165f394f6bedc8dc014c136661cd7690c0cd2ec0
ennvina <vinnywow@aim.com>
2023-05-05 21:21:42 +0200

    Use Settings workaround for TOGC patch

--- options/InterfaceOptionsPanels.lua
+++ options/InterfaceOptionsPanels.lua
@@ -191,6 +191,43 @@ local function applyAllFunc(self, opacityValue, scaleValue, offsetValue, isGlowE
     end
 end
 
+local InterfaceOptions_AddCategory = InterfaceOptions_AddCategory
+local InterfaceOptionsFrame_OpenToCategory = InterfaceOptionsFrame_OpenToCategory
+
+if Settings and Settings.RegisterCanvasLayoutCategory then
+    --[[ Deprecated. 
+    See Blizzard_ImplementationReadme.lua for recommended setup.
+    ]]
+    InterfaceOptions_AddCategory = function(frame, addOn, position)
+        -- cancel is no longer a default option. May add menu extension for this.
+        frame.OnCommit = frame.okay;
+        frame.OnDefault = frame.default;
+        frame.OnRefresh = frame.refresh;
+
+        if frame.parent then
+            local category = Settings.GetCategory(frame.parent);
+            local subcategory, layout = Settings.RegisterCanvasLayoutSubcategory(category, frame, frame.name, frame.name);
+            subcategory.ID = frame.name;
+            return subcategory, category;
+        else
+            local category, layout = Settings.RegisterCanvasLayoutCategory(frame, frame.name, frame.name);
+            category.ID = frame.name;
+            Settings.RegisterAddOnCategory(category);
+            return category;
+        end
+    end
+
+    -- Deprecated. Use Settings.OpenToCategory().
+    InterfaceOptionsFrame_OpenToCategory = function(categoryIDOrFrame)
+        if type(categoryIDOrFrame) == "table" then
+            local categoryID = categoryIDOrFrame.name;
+            return Settings.OpenToCategory(categoryID);
+        else
+            return Settings.OpenToCategory(categoryIDOrFrame);
+        end
+    end
+end
+
 function SpellActivationOverlayOptionsPanel_OnLoad(self)
     self.name = AddonName;
     self.okay = okayFunc;
