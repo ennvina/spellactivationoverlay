fda1a0fda66f9ac456a2b379cd8f1f8511bf6997
ennvina <vinnywow@aim.com>
2024-04-20 14:23:50 +0200

    Package script for Cataclysm

--- _script/package.sh
+++ _script/package.sh
@@ -35,16 +35,15 @@ cp -R changelog.md LICENSE SpellActivationOverlay.* classes components options t
 cd ./_release/wrath || bye "Cannot cd to wrath directory"
 echo -n "Cleaning up wrath directory... "
 # Remove unused textures to reduce the archive size.
-# The list below, SOD_ONLY_TEXTURES, is a list of textures added exclusively for Season of Discovery.
-SOD_ONLY_TEXTURES=(tooth_and_claw
+# The list below, TEXTURES_NOT_FOR_WRATH, is a list of textures added exclusively for Season of Discovery.
+TEXTURES_NOT_FOR_WRATH=(tooth_and_claw
 monk_serpent
 raging_blow
 arcane_missiles_1
 arcane_missiles_2
 arcane_missiles_3
-fulmination
-)
-for texname in ${SOD_ONLY_TEXTURES[@]}
+fulmination)
+for texname in ${TEXTURES_NOT_FOR_WRATH[@]}
 do
     rm -f SpellActivationOverlay/textures/"$texname".* || bye "Cannot cleanup textures from wrath installation"
 done
@@ -69,12 +68,12 @@ sed -i s/'^## Interface:.*'/"## Interface: $VANILLA_BUILD_VERSION"/ SpellActivat
 sed -i '/deathknight/d' SpellActivationOverlay/SpellActivationOverlay.toc || bye "Cannot remove deathknight from TOC file"
 rm -f SpellActivationOverlay/classes/deathknight.lua || bye "Cannot remove deathknight class file"
 # Remove unused textures to reduce the archive size.
-# The list below, WRATH_ONLY_TEXTURES, is based on the contents of
+# The list below, TEXTURES_NOT_FOR_VANILLA, is based on the contents of
 # SpellActivationOverlayDB.debug.unmarked after calling the global
 # function SAO_DB_ComputeUnmarkedTextures() on each and every class
 # on characters logged in with the Classic Era game client.
 # Because these textures are not 'marked', we don't need them.
-WRATH_ONLY_TEXTURES=(master_marksman
+TEXTURES_NOT_FOR_VANILLA=(master_marksman
 molten_core
 art_of_war
 lock_and_load
@@ -86,7 +85,7 @@ predatory_swiftness
 sword_and_board
 killing_machine
 rime)
-for texname in ${WRATH_ONLY_TEXTURES[@]}
+for texname in ${TEXTURES_NOT_FOR_VANILLA[@]}
 do
     rm -f SpellActivationOverlay/textures/"$texname".* || bye "Cannot cleanup textures from vanilla installation"
 done
@@ -95,3 +94,33 @@ echo -n "Zipping vanilla directory... "
 "$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION_TOC_VERSION"-vanilla.zip SpellActivationOverlay || bye "Cannot zip vanilla directory"
 echo
 explorer . # || bye "Cannot open explorer to vanilla directory"
+
+cd "$CWD"/.. || bye "Cannot go to parent directory"
+
+# Release cata version
+rm -rf ./_release/cata || bye "Cannot clean cata directory"
+mkdir -p ./_release/cata/SpellActivationOverlay || bye "Cannot create cata directory"
+cp -R changelog.md LICENSE SpellActivationOverlay.* classes components options textures ./_release/cata/SpellActivationOverlay/ || bye "Cannot copy cata files"
+cd ./_release/cata || bye "Cannot cd to cata directory"
+echo -n "Cleaning up cata directory... "
+# Change Interface version; to know the version of a specific game client, enter: /dump select(4, GetBuildInfo())
+CATA_BUILD_VERSION=40400
+sed -i s/'^## Interface:.*'/"## Interface: $CATA_BUILD_VERSION"/ SpellActivationOverlay/SpellActivationOverlay.toc || bye "Cannot update version of TOC file"
+# Remove unused textures to reduce the archive size.
+# The list below, TEXTURES_NOT_FOR_CATA, is a list of textures added exclusively for Season of Discovery.
+TEXTURES_NOT_FOR_CATA=(tooth_and_claw
+monk_serpent
+raging_blow
+arcane_missiles_1
+arcane_missiles_2
+arcane_missiles_3
+fulmination)
+for texname in ${TEXTURES_NOT_FOR_CATA[@]}
+do
+    rm -f SpellActivationOverlay/textures/"$texname".* || bye "Cannot cleanup textures from wrath installation"
+done
+echo
+echo -n "Zipping cata directory... "
+"$CWD"/zip -9 -r -q SpellActivationOverlay-"$VERSION_TOC_VERSION"-cata.zip SpellActivationOverlay || bye "Cannot zip cata directory"
+echo
+explorer . # || bye "Cannot open explorer to cata directory"
