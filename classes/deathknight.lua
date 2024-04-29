c72a7e3963e3fef74775914b3fa4fc19b2e3622f
ennvina <vinnywow@aim.com>
2024-04-29 02:05:26 +0200

    Use high level effect for DKs

--- classes/deathknight.lua
+++ classes/deathknight.lua
@@ -1,91 +1,75 @@
 local AddonName, SAO = ...
 
-local runeStrike = {
-    name = "rune_strike",
-    project = SAO.WRATH + SAO.CATA,
-    spellID = 56815; -- Rune Strike (ability)
+local deathCoil = 47541;
+local frostStrike = 49143;
+local howlingBlast = 49184;
+local icyTouch = 45477;
+local obliterate = 49020;
+local runeStrike = 56815;
 
-    counter = true,
+local function useRuneStrike()
+    SAO:CreateEffect(
+        "rune_strike",
+        SAO.WRATH + SAO.CATA,
+        runeStrike, -- Rune Strike (ability)
+        "counter",
+        { useName = false }
+    );
+end
 
-    buttons = {{
-        spellID = nil, -- Inherits from effect
-        useName = false,
-    }}
-}
+local function useRime()
+    SAO:CreateEffect(
+        "rime",
+        SAO.WRATH + SAO.CATA,
+        59052, -- Freezing Fog (buff)
+        "aura",
+        {
+            talent = 49188, -- Rime (talent)
+            overlay = { texture = "rime", location = "Top" },
+            buttons = {
+                [SAO.WRATH] = howlingBlast,
+                [SAO.CATA] = { howlingBlast, icyTouch },
+            },
+        }
+    );
+end
 
-local rime = {
-    name = "rime",
-    project = SAO.WRATH + SAO.CATA,
-    spellID = 59052; -- Freezing Fog (buff)
-    talent = 49188; -- Rime (talent)
+local function useKillingMachine()
+    SAO:CreateEffect(
+        "killing_machine",
+        SAO.WRATH + SAO.CATA,
+        51124, -- Killing Machine (buff)
+        "aura",
+        {
+            talent = 51123, -- Killing Machine (talent)
+            overlay = { texture = "killing_machine", location = "Left + Right (Flipped)" },
+            buttons = {
+                [SAO.WRATH] = { icyTouch, frostStrike, howlingBlast },
+                [SAO.CATA] = { frostStrike, obliterate },
+            },
+        }
+    );
+end
 
-    overlays = {{
-        texture = "rime",
-        location = "Top",
-    }},
-
-    buttons = {{
-        project = SAO.WRATH + SAO.CATA,
-        spellID = 49184, -- Howling Blast
-        useName = true,
-    }, {
-        project = SAO.CATA,
-        spellID = 45477, -- Icy Touch
-        useName = true,
-    }},
-}
-
-local killingMachine = {
-    name = "killing_machine",
-    project = SAO.WRATH + SAO.CATA,
-    spellID = 51124; -- Killing Machine (buff)
-    talent = 51123; -- Killing Machine (talent)
-
-    overlays = {{
-        texture = "killing_machine",
-        location = "Left + Right (Flipped)",
-    }},
-
-    buttons = {{
-        project = SAO.WRATH,
-        spellID = 45477, -- Icy Touch
-        useName = true,
-    }, {
-        project = SAO.WRATH + SAO.CATA,
-        spellID = 49143, -- Frost Strike
-        useName = true,
-    }, {
-        project = SAO.WRATH,
-        spellID = 49184, -- Howling Blast
-        useName = true,
-    }, {
-        project = SAO.CATA,
-        spellID = 49020, -- Obliterate
-        useName = true,
-    }},
-}
-
-local suddenDoom = {
-    name = "sudden_doom",
-    project = SAO.CATA,
-    spellID = 81340; -- Sudden Doom (buff)
-    talent = 49018; -- Sudden Doom (talent)
-
-    overlays = {{
-        texture = "sudden_doom",
-        location = "Left + Right (Flipped)",
-    }},
-
-    buttons = {{
-        spellID = 47541, -- Death Coil
-    }},
-}
+local function useSuddenDoom()
+    SAO:CreateEffect(
+        "sudden_doom",
+        SAO.CATA,
+        81340, -- Sudden Doom (buff)
+        "aura",
+        {
+            talent = 49018, -- Sudden Doom (talent)
+            overlay = { texture = "sudden_doom", location = "Left + Right (Flipped)" },
+            button = deathCoil,
+        }
+    );
+end
 
 local function registerClass(self)
-    self:RegisterEffect(runeStrike);
-    self:RegisterEffect(rime);
-    self:RegisterEffect(killingMachine);
-    self:RegisterEffect(suddenDoom);
+    useRuneStrike();
+    useRime();
+    useKillingMachine();
+    useSuddenDoom();
 end
 
 local function loadOptions(self)
